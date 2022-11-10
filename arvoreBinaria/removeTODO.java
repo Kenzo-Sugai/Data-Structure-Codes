d39340a7e4c1d0ac4e755bfde1654f7e65bbd694
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2022-11-09 22:42:22 -0300

    Update removeTODO.java

--- arvoreBinaria/removeTODO.java
+++ arvoreBinaria/removeTODO.java
@@ -1,4 +1,3 @@
-package removearv;
 /**
  * @author      : lopespt (lopespt@$HOSTNAME)
  * @file        : main
@@ -7,15 +6,15 @@ package removearv;
 
 class No {
     public No(int valor) {
-        this.valor = valor;
-        this.esq = null;
-        this.dir = null;
-        this.pai = null;
-    }
-    public int valor;
-    public No esq;
-    public No dir;
-    public No pai;
+    this.valor = valor;
+    this.esq = null;
+    this.dir = null;
+    this.pai = null;
+}
+public int valor;
+public No esq;
+public No dir;
+public No pai;
 }
 
 interface ArvBin {
@@ -29,150 +28,193 @@ class ArvBinBusca implements ArvBin {
     public No raiz = null;
     private int n = 0;
     private void imprimeErd(No a) {
-        if(a != null) {
-            imprimeErd(a.esq);
-            System.out.print(a.valor + "  ");
-            imprimeErd(a.dir);
-        }
-    }
+    if(a != null) {
+    imprimeErd(a.esq);
+    System.out.print(a.valor + "  ");
+    imprimeErd(a.dir);
+}
+}
 
-    public boolean insere(int valor) {
-         No novo = new No(valor);
-        if(raiz == null){
-            raiz = novo;
-            this.n++;
-            return true;
-        }
-        
-        No anterior = null;
-        No atual = raiz;
-        while(atual != null){
-            anterior = atual;
-            if(valor > atual.valor) atual = atual.dir;
-            else if(atual.valor > valor) atual = atual.esq;
-        }
-        
-        novo.valor = valor;
-        
-        if(novo.valor > anterior.valor) anterior.dir = novo;
-        else if(novo.valor < anterior.valor) anterior.esq = novo;
-        this.n++;
-     
-        return true;
-    }
-    public int getN() {
-        return n;
-    }
-    
-    private boolean isEsq(No atual){
-        if(atual.esq == null) return false;
-        else return true;
-    }
-    
-    private boolean removeRec(No atual){
-        if(atual.esq == null && atual.dir == null){ // 0 filhos
-            if(isEsq(atual.pai)){
-                atual.pai.esq = null;
-            }
-            else atual.pai.dir = null;
-            
-        } 
-        else if(atual.esq != null && atual.dir != null){ // 2 filhos
-            No atualsave = atual;
+public boolean insere(int valor) {
+    No novo = new No(valor);
+
+    No anterior = null;
+    No atual = raiz;
+
+    while(atual != null) {
+        anterior = atual;
+        if (valor <= atual.valor)
+            atual = atual.esq;
+        else
             atual = atual.dir;
-            
-            while(atual.esq != null){
-                atual = atual.esq;
-            }
-            
-            atualsave.valor = atual.valor;
-            removeRec(atual);
-        }
-        else{ // 1 filho
-            if(atual.esq == null) atual.pai.dir = atual.dir;
-            else atual.pai.esq = atual.esq;
-        }
-       
-        return true;
-    }
-    
-    public boolean remove(int valor) {
-        No atual = raiz;
-        No anterior = null;
-        while(atual != null){
-            if(atual.valor == valor) break;
-            anterior = atual;
-            if(valor > atual.valor) atual = atual.dir;
-            else if(atual.valor > valor) atual = atual.esq;
-        }
-         
-        removeRec(atual);
-        this.n--;
-        return true;
-        
     }
 
-    public boolean busca(int valor) {
-        No atual = raiz;
-        while(atual != null){
-            if(atual.valor == valor) return true;
-            if(valor > atual.valor) atual = atual.dir;
-            else if(atual.valor > valor) atual = atual.esq;
+    novo.pai = anterior;
+    if (anterior != null) {
+        if (valor <= anterior.valor) {
+            anterior.esq = novo;
+        } else {
+            anterior.dir = novo;
         }
-        return false;
+    } else {
+        raiz = novo;
     }
+    n++;
+    return true;
+}
+public int getN() {
+    return n;
+}
 
-    public void imprime() {
-        imprimeErd(raiz);
-        System.out.println();
-    }
+private void removeCaso1(No atual){
+
+        No pai = atual.pai, filho;
+
+        if(atual.dir != null) filho = atual.dir;
+        else filho = atual.esq;
+
+        if(pai.dir == atual) pai.dir = filho;
+        else pai.esq = filho;
+
+        filho.pai = pai;
+}
+
+private No sucessor(No atual){
+        atual = atual.dir;
+        while(atual.esq != null) atual = atual.esq;
+        return atual;
+}
+
+private void removeCaso2(No atual){
+
+        No filhoRebaixado = sucessor(atual);
+
+        atual.valor = filhoRebaixado.valor;
+        remove(filhoRebaixado);
 
 }
 
-public class main {
+private boolean remove(No atual) {
+
+        if(atual.dir == null && atual.esq == null){ // 0 FILHOS
+            if(atual != raiz){
+                if(atual.pai.dir == atual) atual.pai.dir = null;
+                else atual.pai.esq = null;
+            }
+            else{
+                raiz = null;
+            }
+        }
+        else if(atual.dir != null ^ atual.esq != null){ // 1 FILHO
+            if(atual != raiz) {
+                removeCaso1(atual);
+            }
+            else{
+                if(atual.dir == null) raiz = atual.esq;
+                else raiz = atual.dir;
+            }
+        }
+        else{ // 2 FILHOS
+            removeCaso2(atual);
+
+        }
+
+        return true;
+}
+
+public boolean remove(int valor) {
+    No atual = raiz;
+    while(atual != null) {
+        if (valor < atual.valor)
+            atual = atual.esq;
+        else if (valor > atual.valor)
+            atual = atual.dir;
+        else {
+            n--;
+            return remove(atual);
+        }
+    }
+
+    return false;
+
+}
+
+public boolean busca(int valor) {
+    No atual = raiz;
+    while(atual != null) {
+        if (valor < atual.valor)
+            atual = atual.esq;
+        else if (valor > atual.valor)
+            atual = atual.dir;
+        else
+            return true;
+    }
+
+    return false;
+}
+
+public void imprime() {
+    imprimeErd(raiz);
+    System.out.println();
+}
+
+}
+
+public class Main {
     private static void testeBusca(ArvBinBusca a, int valor) {
-        if (a.busca(valor)) {
-            System.out.println("valor " + valor + " encontrado na arvore");
-        } else {
-            System.out.println("valor " + valor + " NAO encontrado na arvore");
-        }
+    if (a.busca(valor)) {
+    System.out.println("valor " + valor + " encontrado na arvore");
+} else {
+    System.out.println("valor " + valor + " NAO encontrado na arvore");
+}
+}
+private static void testeInsere(ArvBinBusca a, int valor) {
+    if (a.insere(valor)) {
+        System.out.println("valor " + valor + " inserido na arvore");
+        a.imprime();
+        System.out.println("======== Total: " + a.getN() + " nos ===========\n");
     }
-    private static void testeInsere(ArvBinBusca a, int valor) {
-        if (a.insere(valor)) {
-            System.out.println("valor " + valor + " inserido na arvore");
-            a.imprime();
-            System.out.println("======== Total: " + a.getN() + " nos ===========\n");
-        }
+}
+private static void testeRemove(ArvBinBusca a, int valor) {
+    if (a.remove(valor)) {
+        System.out.println("valor " + valor + " removido da arvore");
+        a.imprime();
+        System.out.println("======== Total: " + a.getN() + " nos ===========\n");
+    } else {
+        System.out.println("valor " + valor + " NAO removido da arvore");
     }
-    private static void testeRemove(ArvBinBusca a, int valor) {
-        if (a.remove(valor)) {
-            System.out.println("valor " + valor + " removido na arvore");
-            a.imprime();
-            System.out.println("======== Total: " + a.getN() + " nos ===========\n");
-        }
-    }
-    public static void main(String[] args) {
-        ArvBinBusca a = new ArvBinBusca();
-        testeInsere(a, 51);
-        testeInsere(a, 43);
-        testeInsere(a, 5);
-        testeInsere(a, 53);
-        testeInsere(a, -15);
-        testeInsere(a, 36);
-        testeInsere(a, 17);
-        testeInsere(a, 56);
-        testeInsere(a, 55);
-        testeInsere(a, 74);
-        testeBusca(a, 55);
-        testeBusca(a, 2);
-        testeBusca(a, 15);
-        testeBusca(a, 36);
-        testeBusca(a, 22);
-        testeBusca(a, -5);
-        testeBusca(a, 55);
-        testeBusca(a, 74);
-        testeRemove(a, 51);
-        testeRemove(a, 74);
-        testeRemove(a, -15);
-    }
+}
+public static void main(String[] args) {
+    ArvBinBusca a = new ArvBinBusca();
+    testeInsere(a, 51);
+    testeInsere(a, 43);
+    testeInsere(a, 5);
+    testeInsere(a, 53);
+    testeInsere(a, -15);
+    testeInsere(a, 36);
+    testeInsere(a, 17);
+    testeInsere(a, 56);
+    testeInsere(a, 55);
+    testeInsere(a, 74);
+    testeBusca(a, 55);
+    testeBusca(a, 2);
+    testeBusca(a, 15);
+    testeBusca(a, 36);
+    testeBusca(a, 22);
+    testeBusca(a, -5);
+    testeBusca(a, 55);
+    testeBusca(a, 74);
+    testeRemove(a, 51);
+    testeRemove(a, 43);
+    testeRemove(a, -15);
+    testeRemove(a, 17);
+    testeRemove(a, 56);
+    testeRemove(a, 36);
+    testeRemove(a, 36);
+    testeRemove(a, 55);
+    testeRemove(a, 74);
+    testeRemove(a, 5);
+    testeRemove(a, 53);
+
+}
 }
